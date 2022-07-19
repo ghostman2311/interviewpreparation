0f2583cb417d9d9a858af0fdf756ba44a7eafad5
Nikhil <nkarnwal07@gmail.com>
2022-07-19 22:48:53 +0530

    finding pivot index

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-// Input: nums = [1,2,3,4]
-// Output: [1,3,6,10]
-// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+const arr = [1, 2, 3];
 
-// Input: nums = [1,1,1,1,1]
-// Output: [1,2,3,4,5]
-// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
-
-function runningSum(arr) {
-  let sum = 0;
-  const results = arr.map((item) => {
-    sum = sum + item;
-    return sum;
+const findPivotIndex = (arr) => {
+  let lhs = 0;
+  let rhs = arr.reduce((acc, curr) => {
+    return curr + acc;
   });
 
-  return results;
-}
+  let i = 0;
 
-const arr = [1, 2, 3, 4];
+  while (i < arr.length) {
+    lhs += arr[i];
+    if (lhs === rhs) {
+      return i;
+    }
 
-console.log(runningSum(arr));
+    rhs -= arr[i];
+    i++;
+  }
+
+  return -1;
+};
+
+console.log(findPivotIndex(arr));
